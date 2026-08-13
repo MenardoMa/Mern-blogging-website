3a6d5c5a738248304eb34e9035a130edf065af28
Menardo <menardotera@gmail.com>
2026-08-13 11:19:45 +0100

    Create section Banner Uploaded image for editor component

--- frontend/src/components/blog-editor.component.jsx
+++ frontend/src/components/blog-editor.component.jsx
@@ -1,7 +1,15 @@
 import { Link } from "react-router-dom";
 import logo from "../imgs/logo.png"
+import AnimationWrapper from "../common/page-animation";
+
+import banner from "../imgs/blog-banner.png"
 
 const BlogEditor = () => {
+    
+    const handleBannerUpload = () => {
+        
+    }
+    
     return (
         <>
             <nav className="navbar">
@@ -25,7 +33,27 @@ const BlogEditor = () => {
                 </div>
 
             </nav>
-            
+            <AnimationWrapper>
+                <section>
+                    <div className="mx-auto max-w-[900px] w-full">
+                        <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
+                            <label htmlFor="uploadBanner">
+                                <img
+                                    src={ banner }
+                                    className="z-20"
+                                />
+                                <input 
+                                    type="file" 
+                                    id="uploadBanner"
+                                    accept=".png, .jpg, .jpeg, .webp"
+                                    hidden
+                                    onChange={handleBannerUpload}
+                                />
+                            </label>
+                        </div>
+                    </div>
+                </section>
+            </AnimationWrapper>
         </>
     )
 }
