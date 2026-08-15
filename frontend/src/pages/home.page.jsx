2e09fcf9056f5ace2d28fc4cf47b365963385680
Menardo <menardotera@gmail.com>
2026-08-15 23:12:37 +0100

    fixe structure HomePage

--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -1,6 +1,19 @@
+import AnimationWrapper from "../common/page-animation";
+
 const HomePage = () => {
     return (
-        <h1>Home Page</h1>
+        <AnimationWrapper>
+            <section className="h-cover flex justify-center gap-10">
+                {/* latest blog */}
+                <div className="w-full">
+
+                </div>
+                {/* filter and trending blog */}
+                <div>
+
+                </div>
+            </section>
+        </AnimationWrapper>
     )
 }
 
