e0aa59fda207d82a3ef50fd5554ce05836742e04
Menardo <menardotera@gmail.com>
2026-08-16 19:30:11 +0100

    Create api route search-blogs , permet de Filtrés les tags

--- server/server.js
+++ server/server.js
@@ -392,6 +392,39 @@ server.get("/trending-blogs", (req, res) => {
 })
 
 
+/**
+ * 
+ * End-point Filtre tags
+ * 
+ */
+server.post("/search-blogs", (req, res) => {
+
+    let { tag } = req.body
+    let findQuery = {tags: tag, draft: false }
+    let maxLimit = 5;
+
+    Blog.find( findQuery )
+        .populate(
+            "author",
+            "personal_info.profile_img personal_info.username personal_info.fullname -_id"
+        )
+        .sort({ "activity.total_reads": -1, "activity.total_likes": -1, "publishedAt": -1 }) // trie descroissante
+        .select("blog_id title des banner activity tags publishedAt -_id")                  // on recup sauf -_id
+        .limit(maxLimit)
+        .then(blogs => {
+
+            return res.status(200).json({ blogs })
+
+        })
+        .catch(err => {
+
+            return res.status(500).json({
+                error: err.message
+            })
+
+        })
+
+})
 
 server.listen(PORT, () => {
     console.log('Listening on port ' + PORT)
