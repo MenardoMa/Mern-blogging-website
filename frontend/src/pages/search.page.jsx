677cf823e584b2ee0c20a92c7aed7384d68059c8
Menardo <menardotera@gmail.com>
2026-08-18 10:50:26 +0100

    Create Search Event

--- frontend/src/pages/search.page.jsx
+++ frontend/src/pages/search.page.jsx
@@ -1,6 +1,116 @@
+import { useParams } from "react-router-dom";
+import InPageNavigation from "../components/inpage-navigation.component";
+import { useEffect, useState } from "react";
+import Loader from "../components/loader.component";
+import AnimationWrapper from "../common/page-animation";
+import NoDataMessage from "../components/nodata.component";
+import LoadMoreDataBtn from "../components/load-more.component";
+import BlogPostCard from "../components/blog-post.component";
+import axios from "axios";
+import { filterPaginationData } from "../common/filter-pagination-data";
+
+
 const SearchPage = () => {
+    
+    let { query } = useParams()
+    const [blogs, setBlogs] = useState(null)
+
+    /**
+     * 
+     * Handle Search Blogs
+     * 
+     * @param {*} param0 
+     */
+    const searchBlogs = ({ page = 1, create_new_arr = false }) => {
+
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/search-blogs", {  
+            tag: query, 
+            page 
+        })
+        .then( async ({ data }) => {
+
+            let formatedDate = await filterPaginationData({
+                
+                state: blogs,
+                data: data.blogs,
+                page,
+                counteRoute: "/search-blogs-count",
+                data_to_send: {
+                    tag: query
+                },
+                create_new_arr
+
+            })
+
+            setBlogs(formatedDate)
+
+        })
+        .catch(err => {
+            console.log(err.message)
+        })
+
+    }
+
+    useEffect(() => {
+
+
+        searchBlogs({ page: 1 })
+
+    }, [query])
+
+
+    
     return (
-        <h1>search page</h1>
+        <section className="h-cover flex justify-center gap-10">
+            <div className="w-full">
+                <InPageNavigation
+                    routes={[`Recherche "${query}"`, "Comptes correspondants"]}
+                    defaultHidden={"Comptes correspondants"}
+                >
+
+                    <>
+                            {
+                                blogs === null ? ( 
+                                    <Loader /> 
+                                )
+                                : 
+                                    (
+                                        blogs.results.length ?
+                                        blogs.results.map((blog, i) => {
+                                        return (
+                                        <AnimationWrapper
+                                            key={i}
+                                            transition={{ duration: 1, delay: i * .1 }}
+                                        >
+                                            <BlogPostCard
+                                                content={ blog }
+                                                author={ blog.author.personal_info }
+                                            />
+                                        </AnimationWrapper>
+                                        
+                                    )})
+                                    :
+                                    <NoDataMessage 
+                                        message={"Aucun article trouvé pour cette catégorie."}
+                                    />
+                                )
+                            }
+                            {/* <LoadMoreDataBtn
+                                state={blogs}
+                                fetchDataFun={
+                                    (
+                                        pageState === "Accueil" ? 
+                                        fetchLatestBlogs
+                                        :
+                                        (params) => fetchBlogsByCategory(pageState, params)
+                                    )
+                                }
+                            /> */}
+                    </>
+
+                </InPageNavigation>
+            </div>
+        </section>
     )
 }
 
