0b27fa99199d417fda3df2371d6f647a0c0c67d8
Menardo <menardotera@gmail.com>
2026-08-26 09:30:58 +0100

    Integrate end-point "/get-blog"

--- frontend/src/pages/blog.page.jsx
+++ frontend/src/pages/blog.page.jsx
@@ -1,8 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const BlogPage = () => {
     
     const { blog_id } = useParams()
+    const [blog, setBlog] = useState(null)
+
+    /**
+     * 
+     * Get Blog
+     * 
+     */
+    const fetchBlog = () => {
+
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-blog", {  blog_id })
+        .then(({ data: { blog} }) => {
+
+            setBlog(blog)
+
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
+    }
+
+    useEffect(() => {
+    
+        fetchBlog()
+
+    }, [])
+
+
     
     return (
         <section>
